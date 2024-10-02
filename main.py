9ada4175d97974479d9e8f3dd9a0c37244a0ed3e
Achi80 <149251219+Achi80@users.noreply.github.com>
2024-10-02 20:05:33 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+# 1)
+
+# def factorial(i):
+#     if i == 0 or i == 1:
+#         return 1
+#     else:
+#         return i * factorial(i - 1)
+# print(factorial(5))
+
+# 2)
+
+# def fibonacci(i):
+#     if i <= 0:
+#         return "Error, enter positive integer"
+#     elif i == 1:
+#         return [0]
+#     elif i == 2:
+#         return [0, 1]
+#     number = [0, 1]
+#     for b in range(2, i):
+#         next_value = number[-1] + number[-2]
+#         number.append(next_value)
+#     return number
+
+# print (fibonacci(6))
+
+# 3)
+# import math
+
+# def area(i):
+#     return math.pi*i*i
+
+# print(area(2))
+
+# 4)
+
+# import math
+
+# def triangle(a,b):
+#     sqrt= math.sqrt
+#     hypotenuse = (a*a+b*b)
+#     return (sqrt(hypotenuse))
+
+# print(triangle(1,2))
+
+# 5)
+
+    
